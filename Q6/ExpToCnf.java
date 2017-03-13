ecac4573365a7767528fd676f0adfafd6c9ffaaf
Andrew Downie <andrew_dev@outlook.com>
2017-03-13 16:00:10 -0400

    Added convert method. About to try to get first level of recursion going, by implementing the or operator.

--- Q6/ExpToCnf.java
+++ Q6/ExpToCnf.java
@@ -3,7 +3,7 @@ import java.nio.file.Paths;
 
 public class ExpToCnf{
     public static void main(String[] args){
-        System.out.println("\n\t---- Starting program ExpToCnf ----\n");
+        System.out.println("\n---- Starting program ExpToCnf ----");
 
 
         String expFile_Contents = "";
@@ -28,40 +28,56 @@ public class ExpToCnf{
 
     public static String getCnf(String expression){
         //look for first operator with a open bracket count of zero
+        String result = "";
         int openBrackets = 0;
+        int closedBrackets = 0;
 
         for(int i = 0; i < expression.length(); i++){
             if(expression.charAt(i) == '('){
                 openBrackets++; 
             }
             else if(expression.charAt(i) == ')'){
-                openBrackets--;
+                closedBrackets++;
             }
 
             String propOp = PropositionalOperator(expression, i);
             System.out.println(propOp);
+
             if(propOp != null){
-                if(openBrackets == 0){
+                if(openBrackets == closedBrackets){
                     System.out.println("found PropositionalOperator at index: " + i);
                     String left = expression.substring(0, i);
                     String right = expression.substring(i + propOp.length(), expression.length() - 1);
-                    System.out.println(left);
-                    System.out.println(propOp);
-                    System.out.println(right);
+                    result = Convert(left, right, propOp);
+                    break;
                 }
             }
 
 
         }
 
-        return "this is getCnf";
+        return result;
+    }
+
+    public static String Convert(String left, String right, String operator){
+        System.out.println(left);
+        System.out.println(right);
+        System.out.println(operator);
+
+        if(operator == "<->"){
+            getCnf("(" + left + "^" + right + ")" + " v" + "(~" + left + "^" + "~" + right + ")");
+        }
+
+        return "fart";
     }
 
 
     public static String PropositionalOperator(String expression, int index){
-        if(expression.substring(index, index + 3).equals("<->")){
-            return "<->";
-        }    
+        if(index < expression.length() - 4){
+            if(expression.substring(index, index + 3).equals("<->")){
+                return "<->";
+            }    
+        }
 
 
 
